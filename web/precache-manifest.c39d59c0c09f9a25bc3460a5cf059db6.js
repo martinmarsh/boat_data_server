408775693f5838893dfb060872547e50f10cff26
mm <martin@marshtrio.co.uk>
2019-08-13 02:27:30 +0100

    updated web assests

---
+++ web/precache-manifest.c39d59c0c09f9a25bc3460a5cf059db6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34fc45818dd5f103fb3835a26355dbd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c410efc2687aa78e813",
+    "url": "/static/css/main.587f68e5.chunk.css"
+  },
+  {
+    "revision": "72bbd2498f3f810a28d7",
+    "url": "/static/js/2.056d98d4.chunk.js"
+  },
+  {
+    "revision": "8c410efc2687aa78e813",
+    "url": "/static/js/main.8e80d299.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4794f22a2287f5ce783d6880b3ac9c98",
+    "url": "/static/media/compass_rose.4794f22a.svg"
+  }
+]);
